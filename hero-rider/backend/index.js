afb8016bf0e4d13541e06cd3b975fb074e5696d8
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-03-21 20:04:21 +0600

    rider registration done

--- hero-rider/backend/index.js
+++ hero-rider/backend/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 4000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -8,8 +9,7 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 app.use(express.json());
 app.use(cors());
 
-const uri =
-  "mongodb+srv://heroUser:<password>@cluster0.ivshnpx.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivshnpx.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,13 +21,26 @@ async function run() {
     await client.connect((err) => {
       console.log(err);
     });
+    const ridersCollection = client.db("users").collection("ridersCollection");
+
+    //post rider
+    app.post("/register/rider", async (req, res) => {
+      const query = { email: req.body.email, fullname: req.body.fullname };
+      const availableUser = ridersCollection.findOne(query);
+      if (availableUser) {
+        return res.send({ message: "Rider Already Available" });
+      }
+      const rider = req.body;
+      const result = await ridersCollection.insertOne(rider);
+      res.send(result);
+    });
   } finally {
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server is running!");
 });
 
 app.listen(port, () => {
