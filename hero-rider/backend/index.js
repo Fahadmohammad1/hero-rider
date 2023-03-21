319507d113c867bc50c624818d368690cc9aba64
Fahadmohammad1 <205fahadmohammad@gmail.com>
2023-03-21 22:09:53 +0600

    login api done

--- hero-rider/backend/index.js
+++ hero-rider/backend/index.js
@@ -22,17 +22,85 @@ async function run() {
       console.log(err);
     });
     const ridersCollection = client.db("users").collection("ridersCollection");
+    const learnerCollection = client
+      .db("users")
+      .collection("learnerCollection");
 
-    //post rider
+    //Rider API
+    //Registration
     app.post("/register/rider", async (req, res) => {
       const query = { email: req.body.email, fullname: req.body.fullname };
-      const availableUser = ridersCollection.findOne(query);
+      const availableUser = await ridersCollection.findOne(query);
       if (availableUser) {
         return res.send({ message: "Rider Already Available" });
       }
       const rider = req.body;
       const result = await ridersCollection.insertOne(rider);
       res.send(result);
+    });
+
+    app.get("/rider/info/:email", async () => {
+      const rider = await ridersCollection.findOne({ email: req.params.email });
+      res.send(rider);
+    });
+
+    //login
+    app.get("/login/rider", async (req, res) => {
+      const email = req.body.email;
+      const password = req.body.password;
+      const riderExist = await ridersCollection.findOne({
+        email: email,
+        password: password,
+      });
+
+      if (riderExist) {
+        if (email === riderExist.email && password === riderExist.password) {
+          return res.send({ success: true, message: "Login Successful" });
+        }
+      } else {
+        res.send({ success: false, message: "No Rider Exist" });
+      }
+    });
+
+    //Learner API
+    app.post("/register/learner", async (req, res) => {
+      const query = { email: req.body.email, fullname: req.body.fullname };
+      const availableUser = await learnerCollection.findOne(query);
+      if (availableUser) {
+        return res.send({ message: "User Already Available" });
+      } else {
+        const learner = req.body;
+        const result = await learnerCollection.insertOne(learner);
+        res.send(result);
+      }
+    });
+
+    app.get("/learner/info/:email", async (req, res) => {
+      const learner = await learnerCollection.findOne({
+        email: req.params.email,
+      });
+      res.send(learner);
+    });
+
+    //login
+    app.get("/login/learner", async (req, res) => {
+      const email = req.body.email;
+      const password = req.body.password;
+      const learnerExist = await learnerCollection.findOne({
+        email: email,
+        password: password,
+      });
+
+      if (learnerExist) {
+        if (
+          email === learnerExist.email &&
+          password === learnerExist.password
+        ) {
+          return res.send({ success: true, message: "Login Successful" });
+        }
+      } else {
+        res.send({ success: false, message: "No User Exist" });
+      }
     });
   } finally {
   }
